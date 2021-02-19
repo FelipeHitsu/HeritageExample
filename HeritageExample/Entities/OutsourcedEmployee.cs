3d894582c7e1413253354297f0deae9d2570c3f6
Felipe <felipe.silva.hts@gmail.com>
2021-02-19 15:39:17 -0300

    Commit

---
+++ HeritageExample/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeritageExample.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+
+        public OutsourcedEmployee() { }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge):base(name,hours,valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+    }
+}
